5a274625fca2cd4d78b8e00f0e80a597d6b180ba
Fatima Millanes <alexiaruiz-@hotmail.com>
2022-05-24 18:18:40 -0700

    ya

---
+++ Assets/Scripts/Gameplay/Maggie.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Maggie : Hero
+{
+    protected override void Movement()
+    {
+        //if(gamemanager,.insta.gamemode.GetLeader.gameObject == gameObject)
+        base.Movement();
+        anim.SetFloat("Move", Mathf.Abs(movementValue));
+       
+    }
+}
